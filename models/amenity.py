81c2b018ef4a9f2d4e3ad6330efca8d1e4216445
abel-solo <abel.solo2011@yahoo.com>
2022-12-13 10:49:41 -0800

    amenity modified

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-#!/usr/bin/python3
 from models.base_model import BaseModel, Base
 from models import storage_type
 from sqlalchemy import Column, String
