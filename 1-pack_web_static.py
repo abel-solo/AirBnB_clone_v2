93158426608a90cdad8398764092e8a29ed31419
abel-solo <abel.solo2011@yahoo.com>
2023-01-09 17:18:41 +0000

    PEP8

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,10 +6,11 @@ import time
 
 def do_pack():
     """ generate an tar archive from web_static folder"""
+    time_string = time.strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p versions")
         local("tar -cvzf versions/web_static_{}.tgz web_static/".
-              format(time.strftime("%Y%m%d%H%M%S")))
-        return ("versions/web_static_{}.tgz".format(time.strftime("%Y%m%d%H%M%S")))
+              format(time.string)))
+        return ("versions/web_static_{}.tgz".format(time.string))
     except:
         return None
