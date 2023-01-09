4d2c7f95ba475554031afba56b4967b08cf1fbed
abel-solo <abel.solo2011@yahoo.com>
2023-01-09 18:21:41 +0000

    PEP8

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""script that generates .tgz archive from the contents of the web_static folder"""
+"""
+script that generates .tgz archive from the contents of the web_static folder
+"""
 
 from fabric.api import local
 import time
@@ -7,7 +9,6 @@ import time
 
 def do_pack():
     """Generate an tgz archive from web_static folder"""
-
     try:
         local("mkdir -p versions")
         local("tar -cvzf versions/web_static_{}.tgz web_static/".
