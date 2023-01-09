fc88328b73ebbb833a7c711cbcddca236c491e3c
abel-solo <abel.solo2011@yahoo.com>
2023-01-09 15:13:37 +0000

    documenting a function

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,6 +5,7 @@ import time
 
 
 def do_pack():
+    """ generate an tar archive from web_static folder"""
     try:
         local("mkdir -p versions")
         local("tar -cvzf versions/web_static_{}.tgz web_static/".
