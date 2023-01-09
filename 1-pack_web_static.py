eacb4d710800886514e9f1bc571812cb9d0b9376
abel-solo <abel.solo2011@yahoo.com>
2023-01-09 17:10:01 +0000

    PEP8

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Fabric script that generates a .tgz archive from the contents of the web_static folder
+script that generates a .tgz archive from the contents of the web_static folder
 """
 from fabric.api import local
 import time
