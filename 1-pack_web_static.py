e3bdae1f241eb5bdc5ca269b8102d38a211cf2f1
abel-solo <abel.solo2011@yahoo.com>
2023-01-09 18:11:37 +0000

    PEP8

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,8 +8,7 @@ def do_pack():
     """Generates .tgz archive from web_static folder"""
     try:
         local("mkdir -p versions")
-        local("tar -cvzf versions/web_static_{}.tgz web_static/".
-              format(time.strftime("%Y%m%d%H%M%S")))
+        local("tar -cvzf versions/web_static_{}.tgz web_static/".format(time.strftime("%Y%m%d%H%M%S")))
         return ("versions/web_static_{}.tgz".format(time.strftime("%Y%m%d%H%M%S")))
     except:
         return None
