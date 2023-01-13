096f7199231f6a92dda64e171e82c8870b763398
abel-solo <abel.solo2011@yahoo.com>
2023-01-13 14:48:12 +0000

    [fix] generating new ssh key and servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,29 +3,29 @@
 Script that distributes an archive to your web servers using do_deploy
 """
 
-import os.path
 from fabric.api import local
 from fabric.operations import run, put, sudo
+import os.path
 from fabric.api import env
-env.hosts = ['54.237.65.144', '54.88.134.53']
+env.hosts = ['54.87.231.230', '52.91.116.139']
 
 
 def do_deploy(archive_path):
-    """Deploy an archive to a web server"""
+    """ Distribute an archive to web server"""
     if (os.path.isfile(archive_path) is False):
         return False
 
     try:
-        wsconfig = archive_path.split("/")[-1]
-        wsdir = ("/data/web_static/releases/" + wsconfig.split(".")[0])
+        nconfig = archive_path.split("/")[-1]
+        ndir = ("/data/web_static/releases/" + nconfig.split(".")[0])
         put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}".format(wsdir))
-        run("sudo tar -xzf /tmp/{} -C {}".format(wsconfig, wsdir))
-        run("sudo rm /tmp/{}".format(wsconfig))
-        run("sudo mv {}/web_static/* {}/".format(wsdir, wsdir))
-        run("sudo rm -rf {}/web_static".format(wsdir))
+        run("sudo mkdir -p {}".format(ndir))
+        run("sudo tar -xzf /tmp/{} -C {}".format(nconfig, ndir))
+        run("sudo rm /tmp/{}".format(nconfig))
+        run("sudo mv {}/web_static/* {}/".format(ndir, ndir))
+        run("sudo rm -rf {}/web_static".format(ndir))
         run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(wsdir))
+        run("sudo ln -s {} /data/web_static/current".format(ndir))
         return True
     except:
         return False
